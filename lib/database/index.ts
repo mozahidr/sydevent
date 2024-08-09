588d5936908fd7703f12a0234f6ad9456a2dc172
Mozahid RANA <mozahidpic@gmail.com>
2024-08-09 23:01:17 +1000

    webhook folder name changed

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -13,6 +13,7 @@ export const connectToDatabase = async () => {
         dbName: 'sydevent',
         bufferCommands: false,
     })
+ 
     cached.conn = await cached.promise;
 
     return cached.conn;
